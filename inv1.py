49201c1080b1ba2cb228cf06ca89bac1c20b795f
Uva337 <66168613+Uva337@users.noreply.github.com>
2020-12-21 14:57:14 +0300

    задания

---
+++ inv1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env 2.
+# -*- config: utf-8 -*-
+
+# Напишите рекурсивную функцию, проверяющую правильность расстановки скобок в строке.
+# При правильной расстановке выполняются условия:
+# количество открывающих и закрывающих скобок равно.
+# внутри любой пары открывающая – соответствующая закрывающая скобка, скобки
+# расставлены правильно
+
+
+def par_checker(symbol_string):
+    global symbol, symbol
+    s = []
+    balanced = True
+    index = 0
+
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol in "(":
+            s.append(symbol)
+    else:
+        if len(s) == 0:
+            balanced = False
+        else:
+            top = s.pop()
+            assert isinstance(symbol, object)
+            if not matches(top, symbol):
+                balanced = False
+        index += 1
+
+    return balanced and len(s) == 0
+
+
+def matches(open, close):
+    opens = "("
+    closers = ")"
+    return opens.index(open) == closers.index(close)
+
+
+if __name__ == '__main__':
+
+    print(par_checker('((())())'))
+    print(par_checker(')()'))
---
+++ pr3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- config: utf-8 -*-
+
+
+def factorial(n):
+    product = 1
+    while n > 1:
+        product *= n
+        n -= 1
+    return product
+
+
+def fib(n):
+    a, b = 0, 1
+    while n > 0:
+        a, b = b, a + b
+        n -= 1
+    return a
+
+
+if __name__ == '__main__':
+    import timeit
+
+    print(timeit.timeit("factorial(10)", setup="from __main__ import factorial"))
+    print(timeit.timeit("fib(10)", setup="from __main__ import fib"))
